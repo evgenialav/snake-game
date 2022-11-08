0d3d6e37a5bf7c20389e873eec02b6c62519ba8f
evgenialav <ela@dasburo.com>
2022-11-08 12:02:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+This is a variation of a classic Snake game written in Python using the Turtle module and OOP principles.
+
+Goal:
+- To achieve maximum score (eat as much food as possible) without any collisions.
+
+Rules:
+- Snake grows larger by 1 segment every time it eats the food.
+- Every time it eats the food, the score increases by 1.
+- Snake is not allowed to collide with the walls.
+- Snake is not allowed to collide with it's tail.
+
+How to play:
+- Simply run main.py and enjoy :)
+- You can control the snake's movement by using arrow keys.
---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ('Courier', 18, 'normal')
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.replay = ""
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 275)
+        self.score = 0
+        self.update_board()
+
+    def update_board(self):
+        self.write(f"Score: {self.score}", True, align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.goto(0, 280)
+        self.update_board()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER!", True, align=ALIGNMENT, font=FONT)
